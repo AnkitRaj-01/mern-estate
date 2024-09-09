ed1f1ecf1a5da195192f979078e5adc4910bfb9c
AnkitRaj-01 <ankitboo55@gmail.com>
2024-09-09 18:28:21 +0530

    middleware to handle errors

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js"
+import authRouter from "./routes/auth.route.js";
+
 
 dotenv.config();
 
@@ -25,3 +26,14 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+//creating middleware
+app.use((err,req,res,next)=>{
+    const statusCode =err.statusCode || 500;
+    const message =err.message || "Internal server error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+})
