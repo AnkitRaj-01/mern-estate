6d32af277e4726fbb0e1f9fb48d75890889172e2
Ankit Raj <105981224+AnkitRaj-01@users.noreply.github.com>
2025-02-19 03:51:24 +0530

    userSlice Default

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api':{
+//         target: 'http://localhost:3000',
+//         secure: false,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// })
+
+
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@redux": path.resolve(__dirname, "./src/redux"),
+    },
+  },
   server: {
     proxy: {
-      '/api':{
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         secure: false,
       },
     },
   },
   plugins: [react()],
-})
+});
